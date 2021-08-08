38935372d0744811d95fc0ffb5e187593908b0f8
조현철 <qweas2881@naver.com>
2021-08-08 11:18:07 +0900

    JPA Setting

--- Code/src/main/java/com/whiteship/Application.java
+++ Code/src/main/java/com/whiteship/Application.java
@@ -1,22 +1,35 @@
 package com.whiteship;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
-    public static void main(String[] args) throws SQLException {
-        String url = "jdbc:postgresql://localhost:5432/springdata";
-        String username = "hyeoncheol";
-        String password="pass";
-        
-        try(Connection connection = DriverManager.getConnection(url,username,password)){
-            System.out.println("Connection created: "+connection);
-            String sql = "INSERT INTO ACCOUNT VALUES(1,'hyeon','pass')";
-            try(PreparedStatement statement = connection.prepareStatement(sql)){
-                statement.execute();
-            }
-        }
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
     }
 }
+
+
+//package com.whiteship;
+//
+//import java.sql.Connection;
+//import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+//import java.sql.SQLException;
+//
+//public class Application {
+//    public static void main(String[] args) throws SQLException {
+//        String url = "jdbc:postgresql://localhost:5432/springdata";
+//        String username = "hyeoncheol";
+//        String password="pass";
+//
+//        try(Connection connection = DriverManager.getConnection(url,username,password)){
+//            System.out.println("Connection created: "+connection);
+//            String sql = "INSERT INTO ACCOUNT VALUES(1,'hyeon','pass')";
+//            try(PreparedStatement statement = connection.prepareStatement(sql)){
+//                statement.execute();
+//            }
+//        }
+//    }
+//}
---
+++ Code/src/main/java/com/whiteship/JpaRunner.java
@@ -0,0 +1,29 @@
+package com.whiteship;
+
+import org.hibernate.Session;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+
+@Component
+@Transactional
+public class JpaRunner implements ApplicationRunner {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public void run(ApplicationArguments args) throws Exception {
+        Account account =  new Account();
+        account.setUsername("hyeoncheol");
+        account.setPassword("jpa");
+
+        Session session = entityManager.unwrap(Session.class);
+        session.save(account);
+
+//        entityManager.persist(account);
+    }
+}
