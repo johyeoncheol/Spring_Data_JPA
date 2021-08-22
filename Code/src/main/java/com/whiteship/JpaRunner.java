7cd2031285b83f506072aba2fd305d4251b6ab7a
조현철 <qweas2881@naver.com>
2021-08-22 14:53:17 +0900

    엔티티 상태와 캐스캐이드 정리

--- Code/src/main/java/com/whiteship/JpaRunner.java
+++ Code/src/main/java/com/whiteship/JpaRunner.java
@@ -17,19 +17,38 @@ public class JpaRunner implements ApplicationRunner {
     EntityManager entityManager;
 
     public void run(ApplicationArguments args) throws Exception {
-        Account account =  new Account();
-        account.setUsername("hyeoncheol");
-        account.setPassword("jpa");
+        Post post = new Post();
+        post.setTitle("Spring Data JAP 언제 보나...");
 
-        Study study = new Study();
-        study.setName("Spring Data JPA");
+        Comment comment = new Comment();
+        comment.setComment("빨리 보고 싶어요");
+        post.addComment(comment);
 
-        account.getStudies().add(study);
+        Comment comment1 = new Comment();
+        comment1.setComment("곧 보여드릴게요");
+        post.addComment(comment1);
 
         Session session = entityManager.unwrap(Session.class);
-        session.save(account);
-        session.save(study);
+        session.save(post);
 
-//        entityManager.persist(account);
+
+        //        Account account =  new Account();
+//        account.setUsername("hyeoncheol");
+//        account.setPassword("jpa");
+//
+//        Study study = new Study();
+//        study.setName("Spring Data JPA");
+//
+//        account.getStudies().add(study);
+//
+//        Session session = entityManager.unwrap(Session.class);
+//        session.save(account);
+//        session.save(study);
+//
+////        entityManager.persist(account);
+//
+//        Account hyeoncheol = session.load(Account.class,account.getId());
+//        System.out.println("===========");
+//        System.out.println(hyeoncheol.getUsername());
     }
 }
--- Code/src/main/java/com/whiteship/Post.java
+++ Code/src/main/java/com/whiteship/Post.java
@@ -1,10 +1,7 @@
 package com.whiteship;
 
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,10 +13,35 @@ public class Post {
 
     private String title;
 
-    @OneToMany(mappedBy = "post")
+    @OneToMany(mappedBy = "post" , cascade = CascadeType.PERSIST)
     private Set<Comment> comments = new HashSet<Comment>();
 
     public void addComment(Comment comment){
+        this.getComments().add(comment);
+        comment.setPost(this);
+    }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
     }
 }
